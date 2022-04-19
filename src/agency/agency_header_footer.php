19141173a48ad6b4fc50cc9212c00344111147e2
Maria Semba <moliaiqianyu@gmail.com>
2022-04-19 23:10:27 +0900

    エージェンシー企業のヘッダーとフッターを作った

---
+++ src/agency/agency_header_footer.php
@@ -0,0 +1,24 @@
+<?php
+require('../dbconnect.php');
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+  <header>
+    <p>CRAFT</p>
+    <p>by</p>
+    <img src="../img/syukatudotcom_logo.png" alt="就活.com">
+    <p>お問い合わせ</p>
+  </header>
+
+  <footer>
+    <img src="../img/boozer_logo.png" alt="boozer">
+  </footer>
+</body>
+</html>
