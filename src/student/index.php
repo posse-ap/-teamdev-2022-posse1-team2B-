eb14315516b62912b5dbca41199220f5cff66ca3
Maria Semba <moliaiqianyu@gmail.com>
2022-04-19 22:57:49 +0900

    学生画面のヘッダーとフッターを作った

---
+++ src/student/index.php
@@ -0,0 +1,26 @@
+<?php
+require('../dbconnect.php');
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Top画面</title>
+</head>
+<body>
+  <header>
+    <p>CRAFT</p>
+    <p>by</p>
+    <img src="../img/syukatudotcom_logo_white.png" alt="就活.com">
+    <p>エージェンシー企業向け掲載依頼はこちら</p>
+    <p>お問い合わせ</p>
+  </header>
+
+  <footer>
+    <img src="../img/boozer_logo_png" alt="boozer">
+  </footer>
+</body>
+</html>
---
+++ src/student/student_header_footer.php
@@ -0,0 +1,26 @@
+<?php
+require('../dbconnect.php');
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+  <header>
+    <p>CRAFT</p>
+    <p>by</p>
+    <img src="../img/syukatudotcom_logo.png" alt="就活.com">
+    <p>お問い合わせ</p>
+    <p>エージェンシー企業向け掲載依頼はこちら</p>
+    <a href="./keep.php">keepしてるエージェンシー企業</a>
+  </header>
+
+  <footer>
+    <img src="../img/boozer_logo.png" alt="boozer">
+  </footer>
+</body>
+</html>
