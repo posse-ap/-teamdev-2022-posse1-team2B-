13152779fd416cb2bfa57296be3a555e6ede0efb
Maria Semba <moliaiqianyu@gmail.com>
2022-04-21 22:03:06 +0900

    ヘッダーの書き方を直した

--- src/student/student_header.php
+++ src/student/student_header.php
@@ -11,12 +11,17 @@ require('../dbconnect.php');
 </head>
 <body>
   <header>
+    
     <p>CRAFT</p>
     <p>by</p>
     <img src="../img/syukatudotcom_logo.png" alt="就活.com">
-    <p>お問い合わせ</p>
-    <p>エージェンシー企業向け掲載依頼はこちら</p>
-    <a href="./keep.php">keepしてるエージェンシー企業</a>
+    <nav>
+      <ul>
+        <li>お問い合わせ</li>
+        <li>エージェンシー企業向け掲載依頼はこちら</li>
+        <li><a href="./keep.php">keepしてるエージェンシー企業</a></li>
+      </ul>
+    </nav>
   </header>
 </body>
 </html>
