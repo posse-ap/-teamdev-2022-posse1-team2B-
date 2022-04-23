46da6ad128063aed26f62721fe02f2b9ba3d27b1
Maria Semba <moliaiqianyu@gmail.com>
2022-04-23 23:05:16 +0900

    新規作成ボタンと掲載情報修正依頼ボタンを作った

---
+++ src/agency/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>エージェンシー企業Top画面</title>
+</head>
+<body>
+  <?php include(dirname(__FILE__) . "/agency_header.php");?>
+  <h1>掲載情報登録</h1>
+  <button type="button">新規作成</button>
+  <button type="button">掲載情報修正依頼</button>
+  <?php include(dirname(__FILE__) . "/agency_footer.php");?>
+</body>
+</html>
