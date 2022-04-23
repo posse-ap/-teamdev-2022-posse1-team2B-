a64cfaa958b3c867f483fea1a6cb7e067eed401a
Maria Semba <moliaiqianyu@gmail.com>
2022-04-24 00:11:21 +0900

    ヘッダーとフッターを挿入した

---
+++ src/agency/createcontents.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>新規作成画面</title>
+</head>
+<body>
+  <?php include (dirname(__FILE__) . "/agency_header.php");?>
+  <?php include (dirname(__FILE__) . "/agency_footer.php");?>
+</body>
+</html>
