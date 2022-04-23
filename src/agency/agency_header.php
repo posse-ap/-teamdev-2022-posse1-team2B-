ba2fa056f14b64f0d714f5d2e74b4b6ce712991c
Maria Semba <moliaiqianyu@gmail.com>
2022-04-23 23:12:19 +0900

    ヘッダーとフッターの書き方を修正した

--- src/agency/agency_header.php
+++ src/agency/agency_header.php
@@ -1,15 +1,3 @@
-<?php
-require('../dbconnect.php');
-?>
-
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body>
   <header>
     <div>
       <p>CRAFT</p>
@@ -20,5 +8,3 @@ require('../dbconnect.php');
       <p>お問い合わせ</p>
     </nav>
   </header>
-</body>
-</html>
