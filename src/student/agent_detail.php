f2697528cb9c1caa031360f6dfacb03d6a8e62b6
Maria Semba <moliaiqianyu@gmail.com>
2022-04-28 00:20:56 +0900

    問い合わせ用のinputタグを書いた

--- src/student/agent_detail.php
+++ src/student/agent_detail.php
@@ -9,24 +9,31 @@
 <body>
   <?php include (dirname(__FILE__) . "/student_header.php");?>
   <div>
-    <p>○○企業</p>
-    <dl>
-      <dt>得意な業種</dt>
-      <dd>IT</dd>
-      <dt>対応エリア</dt>
-      <dd>関東、関西</dd>
-      <dt>対象学生</dt>
-      <dd>24卒</dd>
-      <dt>対応企業の規模</dt>
-      <dd>中小企業</dd>
-      <dt>備考</dt>
-      <dd>LINEを用いて親身にサポートします。</dd>
-    </dl>
-    <form action="" method="">
-      <input type="hidden" name="agent_id" value="<?php print($item['agent_id']);?>">
-      <input type="submit" value="キープする">
-    </form>
+    <a href="./keep.php">キープ中の企業</a>
   </div>
+  <div>
+    <div>
+      <p><?php print_r($agent_name);?></p>
+      <dl>
+        <dt>得意な業種</dt>
+        <dd><?php print_r($industry);?></dd>
+        <dt>対応エリア</dt>
+        <dd><?php print_r($supported_area);?></dd>
+        <dt>対象学生</dt>
+        <dd><?php print_r($target_student);?></dd>
+        <dt>対応企業の規模</dt>
+        <dd><?php print_r($corporate_scale);?></dd>
+        <dt>備考</dt>
+        <dd><?php print_r($remarks)?></dd>
+      </dl>
+      <form action="" method="">
+        <input type="hidden" name="agent_id" value="<?php print($item['agent_id']);?>">
+        <input type="submit" value="キープする">
+        <input type="submit" value="エージェンシー企業に問い合わせる">
+      </form>
+    </div>
+  </div>
+
 
   <?php include (dirname(__FILE__) . "/student_footer.php");?>
 </body>
