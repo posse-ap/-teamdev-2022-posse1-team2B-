2d0697b55225c90b7b0cb35e9432c65e3bd07300
Maria Semba <moliaiqianyu@gmail.com>
2022-04-27 22:26:42 +0900

    エージェンシー企業の説明リストを書いた

---
+++ src/student/agent_detail.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php include (dirname(__FILE__) . "/student_header.php");?>
+  <div>
+
+  </div>
+  <div>
+    <p>○○企業</p>
+    <dl>
+      <dt>得意な業種</dt>
+      <dd>IT</dd>
+      <dt>対応エリア</dt>
+      <dd>関東、関西</dd>
+      <dt>対象学生</dt>
+      <dd>24卒</dd>
+      <dt>対応企業の規模</dt>
+      <dd>中小企業</dd>
+      <dt>備考</dt>
+      <dd>LINEを用いて親身にサポートします。</dd>
+    </dl>
+  </div>
+
+  <?php include (dirname(__FILE__) . "/student_footer.php");?>
+</body>
+</html>
