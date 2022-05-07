2297ab0ee517208deff3283fa36edbbeed65136b
Maria Semba <moliaiqianyu@gmail.com>
2022-05-07 15:34:56 +0900

    戻るボタンを作った

--- src/student/keep.php
+++ src/student/keep.php
@@ -9,27 +9,29 @@
 <body>
   <?php include (dirname(__FILE__) . "/student_header.php");?>
   <h1>キープ中のエージェンシー企業</h1>
-      <!-- 閉じるボタン。前のページに戻る -->
-      <button>✕</button>
-        <div>
-          <p><?php print_r($keep_agent["name"]);?></p>
-            <dl>
-              <dt>得意な業種</dt>
-              <dd><?php print_r($keep_agent["industry"]);?></dd>
-              <dt>対応エリア</dt>
-              <dd><?php print_r($keep_agent["supported_area"]);?></dd>
-              <dt>対象学生</dt>
-              <dd><?php print_r($keep_agent["target_student"]);?></dd>
-              <dt>対応企業の規模</dt>
-              <dd><?php print_r($keep_agent["corporate_scale"]);?></dd>
-              <dt>備考</dt>
-              <dd><?php print_r($keep_agent["remarks"]);?></dd>
-            </dl>
-          <form action="./contact.php" method="POST">
-              <input type="hidden" name="agent_id" value="<?php print($agent['agent_id']);?>">
-              <input type="submit" value="エージェンシー企業に問い合わせる">
-          </form>
-        </div>
+    <div>
+      <div>
+        <p><?php print_r($keep_agent["name"]);?></p>
+        <dl>
+          <dt>得意な業種</dt>
+          <dd><?php print_r($keep_agent["industry"]);?></dd>
+          <dt>対応エリア</dt>
+          <dd><?php print_r($keep_agent["supported_area"]);?></dd>
+          <dt>対象学生</dt>
+          <dd><?php print_r($keep_agent["target_student"]);?></dd>
+          <dt>対応企業の規模</dt>
+          <dd><?php print_r($keep_agent["corporate_scale"]);?></dd>
+          <dt>備考</dt>
+          <dd><?php print_r($keep_agent["remarks"]);?></dd>
+        </dl>
+        
+      </div>
+      <form action="./contact.php" method="POST">
+        <button type="button">戻る</button>
+        <input type="hidden" name="agent_id" value="<?php print($agent['agent_id']);?>">
+        <button type="submit">エージェンシー企業に問い合わせる</button>
+      </form>
+    </div>
   <?php include (dirname(__FILE__) . "/student_footer.php");?>
 </body>
 </html>
