cad7a35a37f3e064be4573d1afed983a4142ac83
Maria Semba <moliaiqianyu@gmail.com>
2022-05-08 21:52:47 +0900

    対応エリア別ランキングのHTMLを書いた

--- src/student/index.php
+++ src/student/index.php
@@ -96,6 +96,34 @@
       </li>
     </ol>
   </div>
+  <!-- 対応エリア別ランキングをクリックしたときに表示されるモーダル -->
+  <div id="areaRank">
+    <h1>関東エリアのエージェンシー企業ランキング</h1>
+    <!-- 閉じるボタン -->
+    <button>✕</button>
+    <!-- 画面の右端に表示。クリックするとキープ画面に飛ぶ -->
+    <a href="./keep.php">キープ中の企業</a>
+    <ol>
+    <li>        
+        <p>会社名</p>
+        <p>得意な業種</p>
+        <p>対応エリア</p>
+        <form action="keep.php" method="POST">
+          <input type="hidden" name="agent_id" value="<?php print_r($agent["agent_id"])?>">
+          <button type="submit">キープする</button>
+        </form>
+    </li>
+    <li>        
+        <p>会社名</p>
+        <p>得意な業種</p>
+        <p>対応エリア</p>
+        <form action="keep.php" method="POST">
+          <input type="hidden" name="agent_id" value="<?php print_r($agent["agent_id"])?>">
+          <button type="submit">キープする</button>
+        </form>
+    </li>
+    </ol>
+  </div>
   <?php include (dirname(__FILE__) . "/student_footer.php");?>
 </body>
 </html>
