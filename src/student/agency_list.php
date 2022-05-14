7c4f7c84cbd140049a7a5583b94e4ed9a26b2475
Maria Semba <moliaiqianyu@gmail.com>
2022-05-15 01:42:34 +0900

    エリア別、業種別ランキングのHTMLを別ファイルに分けた

--- src/student/agency_list.php
+++ src/student/agency_list.php
@@ -7,9 +7,10 @@
   <title>Document</title>
 </head>
 <body>
+  <div>
   <!-- 業種別ランキングをクリックした時に表示されるモーダル -->
   <div id="industryRank">
-    <h1 id="industryRankTitle"></h1>
+    <h1>金融</h1>
     <!-- 閉じるボタン -->
     <button id="closeButton">✕</button>
     <!-- 画面の右端に表示。クリックするとキープ画面に飛ぶ -->
@@ -33,6 +34,34 @@
           <button type="submit">キープする</button>
         </form>
       </li>
+    </ol>
+  </div>
+  <!-- 対応エリア別ランキングをクリックしたときに表示されるモーダル -->
+  <div id="areaRank">
+    <h1>関東エリアのエージェンシー企業ランキング</h1>
+    <!-- 閉じるボタン -->
+    <button id="closeButton">✕</button>
+    <!-- 画面の右端に表示。クリックするとキープ画面に飛ぶ -->
+    <a href="./keep.php">キープ中の企業</a>
+    <ol>
+    <li>        
+        <p>会社名</p>
+        <p>得意な業種</p>
+        <p>対応エリア</p>
+        <form action="keep.php" method="POST">
+          <input type="hidden" name="agent_id" value="<?php print_r($agent["agent_id"])?>">
+          <button type="submit">キープする</button>
+        </form>
+    </li>
+    <li>        
+        <p>会社名</p>
+        <p>得意な業種</p>
+        <p>対応エリア</p>
+        <form action="keep.php" method="POST">
+          <input type="hidden" name="agent_id" value="<?php print_r($agent["agent_id"])?>">
+          <button type="submit">キープする</button>
+        </form>
+    </li>
     </ol>
   </div>
 </body>
