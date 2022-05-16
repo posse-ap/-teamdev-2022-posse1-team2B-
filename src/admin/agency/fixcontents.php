6efa96914dffaf3956334ca44b9e335641fb38e7
Maria Semba <moliaiqianyu@gmail.com>
2022-05-16 11:49:49 +0900

    修正依頼画面のHTMLを書いた

---
+++ src/admin/agency/fixcontents.php
@@ -0,0 +1,41 @@
+<!-- 掲載修正依頼画面
+・会社名
+・ 会社住所
+・アイコン画像
+・備考
+・修正を申し込む画面
+-->
+<?php ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php include (dirname(__FILE__) . "/agency_header.php");?>
+  <h1>掲載内容修正申し込み</h1>
+  <form action="./thanks.php">
+    <div>
+      <label for="companyName">会社名</label>
+      <input type="text" id="companyName">
+    </div>
+    <div>
+      <label for="companyAddress">会社住所</label>
+      <input type="text" id="companyAddress">
+    </div>
+    <div>
+      <label for="companyRemarks">備考</label>
+      <input type="text" name="company_remarks" id="companyRemarks">
+    </div>
+    <div>
+      <label for="companyImage">アイコン画像</label>
+      <input type="text" id="companyimage">
+    </div>
+    <button type="submit">修正を申し込む</button>
+  </form>
+  <?php include (dirname(__FILE__) . "/agency_footer.php");?>
+</body>
+</html>
