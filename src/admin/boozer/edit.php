fe95da5482995f13264674282416332784e447ca
Maria Semba <moliaiqianyu@gmail.com>
2022-05-18 02:47:31 +0900

    余計なコメントをｋ消した

--- src/admin/boozer/edit.php
+++ src/admin/boozer/edit.php
@@ -1,87 +1,49 @@
 <?php
 require("../../dbconnect.php");
-//   if(isset($_GET['agent_name'])){
-//   $agent_name = $_GET['agent_name'];
-//   $stmt = $db->prepare('SELECT id FROM agents WHERE agent_name = :agent_name');
-//   $stmt->bindValue(':agent_name', "$agent_name", PDO::PARAM_STR);
-//   $stmt->execute();
-//   $agent_id = $stmt->fetch();
-//   echo $agent_id["id"];
-// }
-//   if(isset($_POST["edit"])) {
-//     // トランザクション開始
-// 		$db->beginTransaction();
-//     try{
-//       $new_agent_name = $_POST["new_agent_name"];
-//       $new_post_number = $_POST["new_post_number"];
-//       $new_prefecture = $_POST["new_prefecture"];
-//       $new_municipalitie = $_POST["new_municipalitie"];
-//       $new_address_number = $_POST["new_address_number"];
-//       // echo $new_agent_name;
-//       // echo $new_post_number;
-//       // echo $new_prefecture;
-//       // echo $new_municipalitie;
-//       // echo $new_address_number;
-//       // $new_remarks = $_POST["new_image"];
-//       // $new_remarks = $_POST["new_remarks"];
-//       // echo $agent_name; クエリ文字取得
-//       // 表示されているエージェンシー企業名と同じエージェンシ企業を取得
-
-//       // $stmt = $db->prepare('UPDATE agents SET agent_name = :new_agent_name, post_number = :new_post_number, prefecture = :new_prefecture, municipalitie = :new_municipalitie, adress_number = :new_address_number WHERE id = :id');
-//       // // $stmt = $db->prepare('UPDATE agents SET agent_name = :new_agent_name, post_number = :new_post_number, prefecture = :new_prefecture, municipalitie = :new_municipalitie, adress_number = :new_address_number WHERE id =:id');
-//       // $stmt = $db->prepare('UPDATE agents SET agent_name = :new_agent_name WHERE id = :id');
-//       $stmt = $db->prepare('UPDATE agents SET agent_name = :new_agent_name WHERE id = :id');
-//       $stmt->bindValue(':id', $agent_id['id'], PDO::PARAM_INT);
-//       $stmt->bindParam(':new_agent_name', $new_agent_name, PDO::PARAM_STR);
-//       // // $stmt->bindValue(':agent_name', "0120123456");
-//       // // $stmt->bindParam(':rikunabi', $agent_name);
-//       // // // $stmt->bindValue(':agent_name', $agent_name);
-//       // $stmt->bindParam(':new_agent_name', $new_agent_name);
-//       // // $stmt->bindParam(':new_post_number', $new_post_number);
-//       // // $stmt->bindParam(':new_prefecture', $new_prefecture);
-//       // // $stmt->bindParam(':new_municipalitie', $new_municipalitie);
-//       // // $stmt->bindParam(':new_address_number', $new_address_number);
-//       $stmt->execute();
-//       // コミット
-// 			$res = $db->commit();
-//       // // $agency_information = $stmt->fetchAll();
-//       // print_r($agency_information[0]["agent_name"]);
-//       // var_dump($agency_information[0]);
-//       // exit();
-//     }
-//   catch(PDOException $e){
-//     //トランザクション取り消し（ロールバック）
-//     $dbh->rollBack();
-//     print('Error:'.$e->getMessage());
-//     echo "<p>登録失敗</p>";
-//     die();
-//   }
-// }
 $id = $_GET['id'];
-if(isset($_POST['edit'])){
-// トランザクション開始
-$db->beginTransaction();
-try {
-  $new_agent_name = $_POST["new_agent_name"];
-  $new_post_number = $_POST["new_post_number"];
-  $new_prefecture = $_POST["new_prefecture"];
-  $new_municipalitie = $_POST["new_municipalitie"];
-  $new_address_number = $_POST["new_address_number"];
-  $stmt = $db->prepare('UPDATE agents SET agent_name = :new_agent_name, post_number = :new_post_number, prefecture = :new_prefecture, municipalitie = :new_municipalitie, adress_number = :new_address_number WHERE id = :id');
-  $stmt->bindValue(':id', $id);
-  $stmt->bindParam(':new_agent_name', $new_agent_name);
-  $stmt->bindParam(':new_agent_name', $new_agent_name);
-  $stmt->bindParam(':new_post_number', $new_post_number);
-  $stmt->bindParam(':new_prefecture', $new_prefecture);
-  $stmt->bindParam(':new_municipalitie', $new_municipalitie);
-  $stmt->bindParam(':new_address_number', $new_address_number);
-  $stmt->execute();
-  // コミット
-  $res = $db->commit();
-  } catch(Exception $e) {
-	// エラーが発生した時はロールバック
-	$pdo->rollBack();
-}
+// if(isset($_POST['edit'])){
+// // トランザクション開始
+// $db->beginTransaction();
+// try {
+//   $new_agent_name = $_POST["new_agent_name"];
+//   $new_post_number = $_POST["new_post_number"];
+//   $new_prefecture = $_POST["new_prefecture"];
+//   $new_municipalitie = $_POST["new_municipalitie"];
+//   $new_address_number = $_POST["new_address_number"];
+//   $stmt = $db->prepare('UPDATE agents SET agent_name = :new_agent_name WHERE id = :id');
+//   $stmt->bindValue(':id', $id);
+//   $stmt->bindParam(':new_agent_name', $new_agent_name);
+//   $stmt->execute();
+//   // $res = $db->commit();
+
+//   $stmt = $db->prepare('UPDATE agents SET post_number = :new_post_number WHERE id = :id');
+//   $stmt->bindValue(':id', $id);
+//   $stmt->bindParam(':new_post_number', $new_post_number);
+//   $stmt->execute();
+//   // $res = $db->commit();
+
+//   $stmt = $db->prepare('UPDATE agents SET prefecture = :new_prefecture WHERE id = :id'); 
+//   $stmt->bindValue(':id', $id);
+//   $stmt->bindParam(':new_prefecture', $new_prefecture);
+//   $stmt->execute();  
+//   // $res = $db->commit();
+
+//   $stmt = $db->prepare('UPDATE agents SET municipalitie = :new_municipalitie WHERE id = :id');
+//   $stmt->bindValue(':id', $id);
+//   $stmt->bindParam(':new_municipalitie', $new_municipalitie);
+//   $stmt->execute();
+//   // $res = $db->commit();
+
+//   $stmt = $db->prepare('UPDATE agents SET adress_number = :new_address_number WHERE id = :id'); 
+//   $stmt->bindValue(':id', $id);
+//   $stmt->bindParam(':new_address_number', $new_address_number);
+//   $stmt->execute();
+//   // コミット
+//   $res = $db->commit();
+//   } catch(Exception $e) {
+// 	// エラーが発生した時はロールバック
+// 	echo "エラーが発生しました";
+// }
   // 更新に成功したらサンクスページへ遷移する
 if( $res ) {
 ?>
