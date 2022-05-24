ac7edc47e56c861147f0fabbf5d241efa2bbbd8a
Shiho Kasai <shihokasai@ShihosMacBook-Pro.local>
2022-05-25 02:09:31 +0900

    time to sleep

--- src/admin/boozer/index.php
+++ src/admin/boozer/index.php
@@ -19,34 +19,39 @@ $agents = $stmt->fetchAll();
 <body>
   <?php include (dirname(__FILE__) . "/boozer_header.php");?>
   <div class="main">
-    <h2 class="pagetitle">掲載企業一覧</h2>
-    <div class="boozerindexinner">
-    <?php 
-    $counter = 0;
-    foreach($agents as $agent): ?>
-    <div>
-      <a href='./agentslist.php?agent_id=<?php echo $agent['id'];?>'>
-        <form action="delete.php" method="POST">
-          <img src="" alt="">
-          <h3><?php echo $agent['agent_name']; ?></h3>
-          <input type="hidden" name="agent_id" value="<?php echo $agent['id'];?>">
-          <input class="editbtn" type="submit" name="edit" formaction="edit.php" value="エージェンシ―企業の掲載を編集">
-          <input class="deletebtn" type="submit" name="delete" value="エージェンシ―企業の掲載を削除">
-
-        </form>
-      </a>
+    <div class="boozerindexouter">
+      <div>
+        <h2 class="pagetitle">掲載企業一覧</h2>
+        <?php 
+        $counter = 0;
+        foreach($agents as $agent): ?>
+        <div>
+          <a href='./agentslist.php?agent_id=<?php echo $agent['id'];?>'>
+            <form action="delete.php" method="POST" class="indexagencybox">
+              <img src="" alt="">
+              <h3><?php echo $agent['agent_name']; ?></h3>
+              <input type="hidden" name="agent_id" value="<?php echo $agent['id'];?>">
+              <div>
+                <input class="editbtn" type="submit" name="edit" formaction="edit.php" value="編集">
+                <input class="deletebtn" type="submit" name="delete" value="削除">
+              </div>
+            </form>
+          </a>
+        </div>
+        <?php 
+          if ($counter >= 2) {break;}
+          $counter++;
+          endforeach;
+        ?>
+        <a  class="inquirybtn" href="./agentslist.php">企業一覧をもっと見る</a>
+      </div>
+      <div class="boozerindexinner">
+        <a class="loginbtn" href="./payment.php">明細確認</a>
+        <a class="inquirybtn" href="./students.php">学生情報</a>
+      </div>
     </div>
-  
+    <a href="./create_contents.php" class="inquirybtn indexbtn">エージェンシーの掲載内容を登録する</a>
   </div>
-  <?php 
-    if ($counter >= 2) {break;}
-    $counter++;
-    endforeach;
-  ?>
-  <a  class="inquirybtn" href="./agentslist.php">企業一覧をもっと見る</a>
-  <a class="loginbtn" href="./payment.php">明細確認</a>
-  <a class="inquirybtn" href="./students.php">学生情報</a>
-  <a href="./create_contents.php">エージェンシー企業の掲載内容を登録する</a>
   <?php include (dirname(__FILE__) . "/boozer_footer.php");?>
   <script src="boozer.js"></script>
 </body>
