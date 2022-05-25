cf24594c99233d433356988d214f9e49688c2a58
Shiho Kasai <shihokasai@ShihosMacBook-Pro.local>
2022-05-25 23:36:27 +0900

    boozer header edit

--- src/admin/boozer/boozer_header.php
+++ src/admin/boozer/boozer_header.php
@@ -8,7 +8,7 @@
       <ul>
         <li><a href="./agentslist.php">掲載企業一覧</a></li>
         <li><a href="./students.php">学生一覧</a></li>
-        <li><a href="payment.php">明細</a></a></li>
+        <li><a href="payment.php">契約企業明細</a></a></li>
       </ul>
     </nav>
 </header>
