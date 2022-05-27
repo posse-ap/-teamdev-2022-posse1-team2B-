0ed088ee36dbdf50acbc2d92e8d8e6a5a8ba67c7
Maria Semba <moliaiqianyu@gmail.com>
2022-05-28 02:54:14 +0900

    選択した条件でエージェンシーを絞り込めるようにした

--- src/student/condition_selection.php
+++ src/student/condition_selection.php
@@ -28,123 +28,110 @@ if(isset($_SESSION['keep'])){
   $_SESSION['time'] = time();
 }
 
-  // 絞り込み検索
-// // $_REQUEST は現在の $_GET、$_POST、$_COOKIE などの内容をまとめた変数
-// $stmt = $db->prepare('select * from characteristic left join agents on agents.id = characteristic.agent_id left join category on characteristic.category_id = category.id left join job_area on characteristic.job_area_id = job_area.id left join target_student on characteristic.target_student_id = target_student.id');
-// $stmt->execute();
-// $information_agents = $stmt->fetchAll();
-// foreach ($information_agents as $information_agent) {}
-
+// 絞り込み検索
 // if(isset($_POST['category'])){
-  foreach($_POST['category'] as $key => $category) {
-    $category_array = array();
-      $category_array[] = "$category";
-      $stmt = $db->prepare('SELECT id FROM category WHERE category_name = :category');
-      $stmt -> bindValue(':category', $category_array[$key]);
-      $stmt->execute();
-      $categories = $stmt->fetchAll();
-      $stmt = $db->prepare('SELECT * FROM characteristic WHERE category_id = :category_id');
-      $stmt -> bindValue(':category_id', $categories[$key]['id']);
-      $stmt->execute();
-      $agent_categories = $stmt->fetchAll();
-      // print_r($agent_category);
-  }
-  foreach($agent_categories as $agent_category) {
-          print_r($agent_category);
-  }
-// characteristic agentsをくっ付ける
 
+//   foreach($_POST['category'] as $category) {
+//     $category_array = array();
 
+//     // print_r($category);
+//     $category_array[] = "$category";
+//     // print_r($category_array[1]);
+
+//       $stmt = $db->prepare('SELECT id FROM category WHERE category_name = :category');
+//       $stmt -> bindValue(':category', $category_array[0]);
+//       $stmt->execute();
+//       $categories = $stmt->fetchAll();
+//       $stmt = $db->prepare('SELECT * FROM characteristic WHERE category_id = :category_id');
+//       $stmt -> bindValue(':category_id', $categories[0]['id']);
+//       $stmt->execute();
+//       $agent_categories = $stmt->fetchAll();
+//   }
+//   // foreach($agent_categories as $agent_category) {
+//   //   $stmt = $db->prepare('SELECT * FROM agents WHERE id = :agent_id');
+//   //   $stmt -> bindValue(':agent_id', $agent_category['agent_id']);
+//   //   $stmt->execute();
+//   //   $agents_information = $stmt->fetch();
+//   //   print_r($agents_information['agent_name']);
+//   // }
 // } 
+
 // if(isset($_POST['job_area'])){
-foreach($_POST['job_area'] as $key => $job_area) {
-  $job_area_array = array();
-    $job_area_array[] = "$job_area";
-    $stmt = $db->prepare('SELECT id FROM job_area WHERE area = :job_area');
-    $stmt -> bindValue(':job_area', $job_area_array[$key]);
-    $stmt->execute();
-    $areas = $stmt->fetchAll();
-    $stmt = $db->prepare('SELECT * FROM characteristic WHERE job_area_id = :area_id');
-    $stmt -> bindValue(':area_id', $areas[$key]['id']);
-    $stmt->execute();
-    $agent_job_areas = $stmt->fetchAll();
-    // print_r($agent_category);
-}
-foreach($agent_categories as $agent_category) {
-        print_r($agent_category);
-} 
+// foreach($_POST['job_area'] as $key => $job_area) {
+//   $job_area_array = array();
+//     $job_area_array[] = "$job_area";
+//     $stmt = $db->prepare('SELECT id FROM job_area WHERE area = :job_area');
+//     $stmt -> bindValue(':job_area', $job_area_array[$key]);
+//     $stmt->execute();
+//     $areas = $stmt->fetchAll();
+//     $stmt = $db->prepare('SELECT * FROM characteristic WHERE job_area_id = :area_id');
+//     $stmt -> bindValue(':area_id', $areas[$key]['id']);
+//     $stmt->execute();
+//     $agent_job_areas = $stmt->fetchAll();
+// }
+// foreach($agent_categories as $agent_category) {
+//         print_r($agent_category);
 // } 
+// }
 // if(isset($_POST['target_student'])){
+// foreach($_POST['target_student'] as $key => $target_student) {
+//   $target_student_array = array();
+//     $target_student_array[] = "$target_student";
+//     $stmt = $db->prepare('SELECT id FROM target_student WHERE graduation_year = :target_student');
+//     $stmt -> bindValue(':target_student', $target_student_array[$key]);
+//     $stmt->execute();
+//     $targets = $stmt->fetchAll();
+//     $stmt = $db->prepare('SELECT * FROM characteristic WHERE target_student_id = :target_id');
+//     $stmt -> bindValue(':target_id', $targets[$key]['id']);
+//     $stmt->execute();
+//     $target_students = $stmt->fetchAll();
+// }
+// foreach($agent_categories as $agent_category) {
+//         print_r($agent_category);
+// }
+// }
 
-foreach($_POST['target_student'] as $key => $target_student) {
-  $target_student_array = array();
+// →これだと、同時にカテゴリーとエリアを選択された時にできない
+// https://www.suzuco.net/entry/php-search/#i-8
+if(isset($_POST["search"])) {
+$where = array();
+if(isset($_POST['category'])) {
+  foreach($_POST['category'] as $category) {
+    $category_array = array();
+    // print_r($category);
+    $category_array[] = "$category";
+    // print_r($category_array[1]);
+    $stmt = $db->prepare('SELECT id FROM category WHERE category_name = :category');
+    $stmt -> bindValue(':category', $category_array[0]);
+    $stmt->execute();
+    $categories = $stmt->fetchAll();
+    $where[] = 'category_id =' . $categories[0]['id'];
+}}
+
+if(isset($_POST['job_area'])) {
+  foreach($_POST['job_area'] as $key => $job_area) {
+      $job_area_array = array();
+      $job_area_array[] = "$job_area";
+      $stmt = $db->prepare('SELECT id FROM job_area WHERE area = :job_area');
+      $stmt -> bindValue(':job_area', $job_area_array[$key]);
+      $stmt->execute();
+      $areas = $stmt->fetchAll();
+      $where[] = 'job_area_id =' . $areas[0]['id'];
+    }
+}
+if(isset($_POST['target_student'])) {
+  foreach($_POST['target_student'] as $key => $target_student) {
     $target_student_array[] = "$target_student";
     $stmt = $db->prepare('SELECT id FROM target_student WHERE graduation_year = :target_student');
     $stmt -> bindValue(':target_student', $target_student_array[$key]);
     $stmt->execute();
     $targets = $stmt->fetchAll();
-    $stmt = $db->prepare('SELECT * FROM characteristic WHERE target_student_id = :target_id');
-    $stmt -> bindValue(':target_id', $targets[$key]['id']);
-    $stmt->execute();
-    $target_students = $stmt->fetchAll();
+    $where[] = 'target_student_id =' . $targets[0]['id'];
+  }
 }
-foreach($agent_categories as $agent_category) {
-        print_r($agent_category);
-} 
-// }
-// ベストではない
-// →カテゴリーが日本語→よろしくない
-// →カテゴリーの名前が変わった時に、エージェンシー企業のテーブル自体も変える
-// →カテゴリーとエージェントのテーブルを分けるべき
-// →中間テーブルを作ってつないであげる
-// if(isset($_POST["job_area"])){
-// switch($search) 
-// {
-//   case $_POST['category']:
-//     echo($_POST['category']);
-//     $category_array = array();foreach($_POST['category'] as $key => $category) {
-//     $category_array[] = "$category";
-//     $stmt = $db->prepare('SELECT * FROM agents WHERE category = :category');
-//     $stmt -> bindValue(':category', $category_array[$key]);
-//     $stmt->execute();
-//     $categories = $stmt->fetchAll();      
-//   }
-//   case $_POST['job_area']:
-//     $job_area_array = array();
-//     foreach($_POST['job_area'] as $key => $job_area) {
-//         $job_area_array[] = "$job_area";
-//         $stmt = $db->prepare('SELECT * FROM agents WHERE job_area = :job_area');
-//         $stmt -> bindValue(':job_area', $job_area_array[$key]);
-//         $stmt->execute();
-//         $areas = $stmt->fetchAll();    
-//         print_r($areas);
-//     }
-//   case $_POST['target_student']:
-//     $category_array = array();
-//     foreach($_POST['target_student'] as $key => $target_student) {
-//         $target_student_array[] = "$target_student";
-//         $stmt = $db->prepare('SELECT * FROM agents WHERE target_student = :target_student');
-//         $stmt -> bindValue(':target_student', $target_student_array[$key]);
-//         $stmt->execute();
-//         $students = $stmt->fetchAll();    
-//         print_r($students);
-//     }
-//   break;
-//   default:
-//     print_r('該当する企業はありません');
-// }
-// }
+print_r($where);
 
-
-
-  
-
-// $where = ""
-// foreach($_REQUEST['category'] as $key => $category) {
-//   $where[] = "category = '$category'"
-// }
-// $print_r($where);
-// }
+}
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -165,6 +152,22 @@ foreach($agent_categories as $agent_category) {
     <h1>絞り込み結果</h1>
     <a href="./condition_selection.php">✕</a>
     <a href="./keep.php">キープ中の企業</a>
+    <?php 
+      if (count($where) > 0) 
+        {
+          $sql = "SELECT * FROM characteristic WHERE " . implode(' AND ', $where);
+          // echo $sql;
+          $stmt = $db->prepare($sql);
+          $stmt->execute();
+          $rows=$stmt->fetchAll();
+          // print_r($rows);
+        foreach($rows as $row) {
+          print_r($row);
+          $stmt = $db->prepare('SELECT * FROM agents WHERE graduation_year = :target_student');
+          $stmt -> bindValue(':target_student', $target_student_array[$key]);
+          $stmt->execute();
+      
+    ?>
     <ul>
       <li>
         <a href="./agent_detail.php">
@@ -172,22 +175,30 @@ foreach($agent_categories as $agent_category) {
           <img src="../img/<?php ?>.png" alt="エージェンシー企業">
           <dl>
             <dt>得意な業種</dt>
-            <dd><?php     print_r($categories);?></dd>
+            <dd><?php print_r($row['category_id']);?></dd>
             <dt>対応エリア</dt>
-            <dd><?php print_r($areas);?></dd>
+            <!-- <dd><?php// print_r($areas);?></dd> -->
             <dt>対象学生</dt>
-            <dd><?php print_r($students);?></dd>
+            <!-- <dd><?php //print_r($students);?></dd> -->
             <dt>対応企業の規模</dt>
-            <dd><?php ?></dd>
+            <!-- <dd><?php ?></dd> -->
           </dl>
           <form action="./keep.php" method="POST">
-            <input type="hidden" name="agent_id" value="<?php print_r($agents[0]['id']);?>">
+            <input type="hidden" name="agent_id" value="">
             <button type="submit" class="keepbtn">キープする</button>
             <button type="submit" formaction="./contact.php" class="inquirybtn">エージェンシー企業に問い合わせる</button>
           </form>
         </a>
       </li>
     </ul>
+      <?php
+
+      }}
+      else{
+        echo '該当する企業はありません';
+      }
+    ?>
+    
   </div>
   <!-- こだわり条件から探すをクリックした場合に表示 -->
   <?php else:?>
