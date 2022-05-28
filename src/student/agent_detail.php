195b0493e88342f7d5202b08f52b582cec0fb2f5
Shiho Kasai <shihokasai@ShihosMacBook-Pro.local>
2022-05-28 18:28:56 +0900

    css complete

--- src/student/agent_detail.php
+++ src/student/agent_detail.php
@@ -41,11 +41,11 @@ if (isset($_SESSION['keep'])) {
   <?php include(dirname(__FILE__) . "/student_header.php"); ?>
   <div class="main">
     <div>
-      <a href="./keep.php" class="keepbtn">キープ中の企業</a>
+      <a href="./keep.php" class="inquirybtn">キープ中の企業</a>
     </div>
     <!-- 閉じるボタン。前のページに戻る -->
     <?php
-    echo ('<a href=' . '"javascript:history.back()"' . '>戻る</a>');
+    echo ('<a href=' . '"javascript:history.back()"' . ' class="returnbtn">戻る</a>');
 
     $stmt = $db->prepare('SELECT * FROM agents WHERE id = :id');
     $id = $_GET["id"];
@@ -53,57 +53,68 @@ if (isset($_SESSION['keep'])) {
     $stmt->execute();
     $agents = $stmt->fetchAll();
     ?>
-    <div>
-      <p>
-        <?php print_r($agents[0]["agent_name"]); ?>
-      </p>
-      <img src="../img/companylogo/<?php print_r($agents[0]['image']); ?>" alt="エージェンシー企業の写真">
+    <div class="agentdetailinner">
+      <div class="agentheader">
+        <img src="../img/companylogo/<?php print_r($agents[0]['image']); ?>" alt="エージェンシー企業の写真">
+        <p>
+          <?php print_r($agents[0]["agent_name"]); ?>
+        </p>
+      </div>
+      <div class="agentinfo">
+        <div>
+          <dt>会社HP：</dt>
+          <dd><a href="<?php print_r($agents[0]["url"]); ?>" target="_blank">リンクはこちら</a></dd>
+        </div>
+        <div>
+          <dt>問い合わせ電話番号：</dt>
+          <dd><?php print_r($agents[0]["tel_number"]); ?></dd>
+        </div>  
+        <div>
+          <dt>住所：</dt>
+          <dd><?php print_r($agents[0]["post_number"]); ?></dd>
+          <dd><?php print_r($agents[0]["prefecture"]);?></dd>
+          <dd><?php print_r($agents[0]["municipalitie"]);?></dd>
+          <dd><?php print_r($agents[0]["adress_number"]);?></dd>
+        </div>
 
-      <dt>会社HP</dt>
-      <dd><a href="<?php print_r($agents[0]["url"]); ?>" target="_blank"><?php print_r($agents[0]["url"]); ?></a></dd>
-      <dt>問い合わせ電話番号</dt>
-      <dd><?php print_r($agents[0]["tel_number"]); ?></dd>
-      <dt>住所</dt>
-      <dd><?php print_r($agents[0]["post_number"]); ?></dd>
-      <dd><?php print_r($agents[0]["prefecture"]);?></dd>
-      <dd><?php print_r($agents[0]["municipalitie"]);?></dd>
-      <dd><?php print_r($agents[0]["adress_number"]);?></dd>
+        <dl>
+          <?php
+          $stmt = $db->prepare('select * from characteristic left join agents on characteristic.agent_id = agents.id right join job_area on characteristic.job_area_id = job_area.id where agent_id = :agent_id');
+          $stmt->bindValue(':agent_id', $id);
+          $stmt->execute();
+          $matched_job_area = $stmt->fetchAll();
 
-      <dl>
-        <?php
-        $stmt = $db->prepare('select * from characteristic left join agents on characteristic.agent_id = agents.id right join job_area on characteristic.job_area_id = job_area.id where agent_id = :agent_id');
-        $stmt->bindValue(':agent_id', $id);
-        $stmt->execute();
-        $matched_job_area = $stmt->fetchAll();
+          $stmt = $db->prepare('select * from characteristic left join agents on characteristic.agent_id = agents.id right join category on characteristic.category_id = category.id where agent_id = :agent_id');
+          $stmt->bindValue(':agent_id', $id);
+          $stmt->execute();
+          $matched_category = $stmt->fetchAll();
 
-        $stmt = $db->prepare('select * from characteristic left join agents on characteristic.agent_id = agents.id right join category on characteristic.category_id = category.id where agent_id = :agent_id');
-        $stmt->bindValue(':agent_id', $id);
-        $stmt->execute();
-        $matched_category = $stmt->fetchAll();
-
-        $stmt = $db->prepare('select * from characteristic left join agents on characteristic.agent_id = agents.id right join target_student on characteristic.target_student_id = target_student.id where agent_id = :agent_id');
-        $stmt->bindValue(':agent_id', $id);
-        $stmt->execute();
-        $matched_target_student = $stmt->fetchAll();
-
-
-        ?>
-
-        <dt>対応エリア</dt>
-        <dd><?php print_r($matched_job_area[0]['area']); ?></dd>
-
-        <dt>得意な業種</dt>
-        <dd><?php print_r($matched_category[0]['category_name']); ?></dd>
-        
-        <dt>対象学生</dt>
-        <dd><?php print_r($matched_target_student[0]['graduation_year']); ?></dd>
-        </dd>
-        <dt>備考</dt>
-        <dd><?php print_r($agents[0]["detail"]); ?></dd>
-      </dl>
+          $stmt = $db->prepare('select * from characteristic left join agents on characteristic.agent_id = agents.id right join target_student on characteristic.target_student_id = target_student.id where agent_id = :agent_id');
+          $stmt->bindValue(':agent_id', $id);
+          $stmt->execute();
+          $matched_target_student = $stmt->fetchAll();
 
 
-
+          ?>
+          <div>
+            <dt>対応エリア：</dt>
+            <dd><?php print_r($matched_job_area[0]['area']); ?></dd>
+          </div>
+          <div>
+            <dt>得意な業種：</dt>
+            <dd><?php print_r($matched_category[0]['category_name']); ?></dd>
+          </div>
+          <div>
+            <dt>対象学生：</dt>
+            <dd><?php print_r($matched_target_student[0]['graduation_year']); ?></dd>
+            </dd>
+          </div>
+          <div>
+            <dt>備考：</dt>
+            <dd><?php print_r($agents[0]["detail"]); ?></dd>
+          </div>
+        </dl>
+      </div>
       <form action="" method="POST">
         <?php
         if (isset($keeps[$agent['id']]) === true) :
