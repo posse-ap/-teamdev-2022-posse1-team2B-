a27ebabd2ba822c3fb4ffcb367b6ee1565e30f85
asaka0408 <sakanadaisuki0408@softbank.ne.jp>
2022-05-29 11:55:31 +0900

    修正内容反映　空白も反映してしまう

--- src/admin/boozer/edit.php
+++ src/admin/boozer/edit.php
@@ -25,6 +25,7 @@ $target_students = $stmt->fetchAll();
 
 
 if (isset($_POST['edit_entry'])) {
+
   $id = $_POST['agent_id'];
   $new_name = $_POST['new_name'];
   $new_url = $_POST['new_url'];
@@ -36,9 +37,6 @@ if (isset($_POST['edit_entry'])) {
   $new_address_number = $_POST['new_address_number'];
   $new_image = $_POST['new_image'];
   $new_detail = $_POST['new_detail'];
-
-
-  // $stmt = $db->prepare('UPDATE agents SET agent_name = :new_agent_name WHERE id = 1');
 
   // トランザクション開始
   $db->beginTransaction();
@@ -94,25 +92,25 @@ if (isset($_POST['edit_entry'])) {
     );
 
     $stmt = $db->prepare('SELECT id FROM category where category_name = :category_name');
-    $stmt->bindValue(':category_name', $_POST["category"]);
+    $stmt->bindValue(':category_name', $_POST["new_category"]);
     $stmt->execute();
     $category_id = $stmt->fetchAll();
 
     $stmt = $db->prepare('SELECT id FROM job_area where area = :area');
-    $stmt->bindValue(':area', $_POST["job_area"]);
+    $stmt->bindValue(':area', $_POST["new_job_area"]);
     $stmt->execute();
     $job_area_id = $stmt->fetchAll();
 
     $stmt = $db->prepare('SELECT id FROM target_student where graduation_year = :graduation_year');
-    $stmt->bindValue(':graduation_year', $_POST["target_student"]);
+    $stmt->bindValue(':graduation_year', $_POST["new_target_student"]);
     $stmt->execute();
     $target_student_id = $stmt->fetchAll();
 
-    $statement->bindValue(':id', $id);
-    $statement->bindParam(':category_id', $category_id);
-    $statement->bindParam(':job_area_id', $job_area_id);
-    $statement->bindParam(':target_student_id', $target_student_id);
-    $statement->execute();
+    $stmt->bindValue(':agent_id', $id);
+    $stmt->bindParam(':category_id', $category_id[0]['id']);
+    $stmt->bindParam(':job_area_id', $job_area_id[0]['id']);
+    $stmt->bindParam(':target_student_id', $target_student_id[0]['id']);
+    $stmt->execute();
     $res = $db->commit();
   } catch (PDOException $e) {
     // エラーが発生した時トランザクションが開始したところまで巻き戻せる
@@ -136,8 +134,7 @@ if (isset($_POST['edit_entry'])) {
   <?php include(dirname(__FILE__) . "/boozer_header.php"); ?>
   <div class="main">
     <h2 class="pagetitle">掲載内容修正</h2>
-<<<<<<< HEAD
-    <form action="../../thanks.php" method="POST" class="inputform">
+    <form action="edit.php" method="POST" class="inputform">
       <dd>会社名</dd>
       <dt><input name='new_name' type="text"></dt>
       <dd>企業サイトのURL</dd>
@@ -182,27 +179,9 @@ if (isset($_POST['edit_entry'])) {
       <dt><input name='new_image' type="file"></dt>
       <dd>備考</dd>
       <dt><textarea name="new_detail" id="detail" cols="30" rows="10"></textarea></dt>
-      </dl>
-      <input type="submit" class="submitbtn" name="edit_entry" value="修正完了">
-      <input type="hidden" name="agent_id" value="<?php echo $_POST['agent_id']; ?>">
-=======
-    <form action="" method="POST" class="inputform">
-      <dd>会社名</dd><dt><input type="text" name="new_agent_name"></dt>
-      <dd>企業サイトのURL</dd><dt><input type="text" name='new_url' required></dt>
-      <dd>電話番号</dd><dt><input name='new_tel_number' type='tel' required></dt>
-      <dd>郵便番号</dd><dt><input type="text" name="new_post_number" pattern="\d{3}-?\d{4}"></dt>
-      <dd>都道府県</dd><dt><input type="text" name="new_prefecture"></dt>
-      <dd>市区町村</dd><dt><input type="text" name="new_municipalitie"></dt>
-      <dd>町域・番地</dd><dt><input type="text" name="new_address_number"></dt>
-      <dd>通知先メールアドレス</dd><dt><input name='new-notification_email' type='email' required></dt>
-      <dd>得意な業種</dd><dt><input name='new_category' type='text' required></dt>
-      <!-- <dd>掲載期間</dd><dt><input type="number" name=""></dt>
-            掲載期間、アイコン画像、備考→agentsテーブルに入ってないけど、どうする？-->
-      <!-- <dd>アイコン画像</dd><dt><input type="file" name="new_image"></dt> -->
-      <dd>備考（アピールポイントなど）</dd><dt><textarea name="new_remarks" id="" cols="30" rows="10"></textarea></dt>
-      <input type="hidden" name="agent_id" value= "<?php echo $_POST['agent_id']; ?>">
-      <input class="submitbtn ignore" name="edit" type="submit" value="修正完了">
->>>>>>> c5d7cb75af27d277d20d0c885aeb6486bce22f79
+    </dl>
+    <input type="hidden" name="agent_id" value="<?php echo $_POST['agent_id']; ?>">
+    <input type="submit" class="submitbtn" name="edit_entry" value="修正完了">
       <a href='javascript:history.back()' class="returnbtn">戻る</a>
     </form>
   </div>
